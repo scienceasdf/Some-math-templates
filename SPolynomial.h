1fd90fc652e2f92679e81c168652576c4bde844b
scienceasdf <scienceasdf@126.com>
2016-06-24 13:13:30 +0800

    Update SPolynomial.h

--- SPolynomial.h
+++ SPolynomial.h
@@ -57,5 +57,81 @@ public:
     std::valarray<numArea> coeArray;
     polynomial()
 }
+template<class T>   // Written without any tests.
+class polynomial{
+private:
+    T* array;
+    int degree;
+public:
+    polynomial() {array=new T; T=0;}
+    polynomial(T val);
+    polynomial(const T val, size_t n); // Construct a (n-1) degree polynomial, all coefficients of val.
+    polynomial(const T* p, size_t n);   // Construct a (n-1) degree polynomial, coefficients from the T pointer starts and on.
+    polynomial(const polynomial& poly); // Copy a polynomial.
+    ~polynomial() {delete [] array;}
+
+    T operator[] (size_t n) const {return array[n];}
+    T& operator[] (size_t n) {return array[n];}
+
+    polynomial& operator*=(const T arg) {std::for_each(begin(),end(),[arg] (T& s){s*=arg;})
+    polynomial& operator/=(const T arg) {std::for_each(begin(),end(),[arg] (T& s){s/=arg;})
+
+    int getDegree() {return degree;}
+    void reDegree(size_t n) { delete [] array; array=new T[n+1];}   
+    // Caution! This operation will delete all the former elements!
+                                     
+    T* begin() {return array;}
+    T* end() {return array+degree+1;}
+
+    T calc(T val) {return polyCalc(begin(),end(),val);}
+
+};
+
+template<class T>
+polynomial<T>::polynomial(T val)
+{
+    array=new T;
+    *array=val;
+}
+
+template<class T>
+polynomial<T>::polynomial(const T val, size_t n)
+{
+    array=new T[n];
+    for(int i=0;i<n;++i){
+        array[n]=val;
+    }
+}
+
+template<class T>
+polynomial<T>::polynomial(const T* p, size_t n)
+{
+    array=new T[n];
+    for(int i=0;i<n;++i){
+        array[n]=p[n];
+    }
+}
+
+template<class T>
+polynomial<T>::polynomial(const polynomial& poly)
+{
+    int n=poly.getDegree()+1;
+    array=new T[n];
+    for(int i=0;i<n;++i){
+        array[n]=poly[n];
+    }
+}
+
+template<class T>
+T polynomial<T>::operator [](size_t n)
+{
+    return array[n];
+}
+
+template<class T>
+T& polynomial<T>::operator [](size_t n)
+{
+    return array[n];
+}
 
 #endif // SPOLYNOMIAL_H
