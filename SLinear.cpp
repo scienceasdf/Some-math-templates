0675d9363b2d95596bfc2532655a87eb2ba561d9
scienceasdf <scienceasdf@126.com>
2016-06-29 13:24:36 +0800

    Update SLinear.cpp

--- SLinear.cpp
+++ SLinear.cpp
@@ -2,20 +2,29 @@
  */
 double* root(double* a, double* b, double* c, double* d, int n)
 {
-    double* alpha=new double[n-1];
-    double* beta;
-    beta=new double[n];
-    beta[0]=b[0];
-    for(int i=0;i<=n-2;++i){
-        alpha[i]=a[i]/beta[i];
-        beta[i+1]=b[i+1]-alpha[i]*c[i];
+    if(n==1){
+        double* x=new double[1];
+        x[0]=d[0]/b[0];
+        return x;
     }
-    double* x=new double[n];
-    x[0]=d[0];
-    for(int i=1;i<=n-1;++i) {x[i]=d[i]-alpha[i-1]*(x[i-1]); }
-    x[n-1]=beta[n-1];
-    for(int i=n-1;i>=0;--i) {x[i]=(x[i]-c[i]*x[i+1])/beta[i];}
-    delete [] alpha;
-    delete [] beta;
-    return x;
+    else{
+        double* alpha=new double[n-1];
+        double* beta;
+        beta=new double[n];
+        beta[0]=b[0];
+        for(int i=0;i<=n-2;++i){
+            alpha[i]=a[i]/beta[i];
+            beta[i+1]=b[i+1]-alpha[i]*(c[i]);
+        }
+        double* x=new double[n],*y=new double[n];
+        y[0]=d[0];
+        for(int i=1;i<=n-1;++i) y[i]=d[i]-alpha[i-1]*(y[i-1]);
+        x[n-1]=beta[n-1];
+        for(int i=n-1;i>=0;--i) x[i]=(y[i]-c[i]*x[i+1])/beta[i];
+        delete [] y;
+        delete [] alpha;
+        delete [] beta;
+        return x;
+    }
 }
+
