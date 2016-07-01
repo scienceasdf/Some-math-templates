94f9eb549950756c30827a2a79a958a6c54a65c7
scienceasdf <scienceasdf@126.com>
2016-07-01 21:16:27 +0800

    Update RungeKuttaTest1.cpp

--- test/RungeKuttaTest1.cpp
+++ test/RungeKuttaTest1.cpp
@@ -8,15 +8,40 @@ double* f2(double* x)
     double* res=new double[2];
     res[0]=1.0;
     res[1]=x[0];
+    return res;
+}
+
+double* f1(double* x)
+{
+    double* res=new double[3];
+    res[0]=1.0;
+    res[1]=x[2];
+    res[2]=x[1];
+    return res;
 }
 
 int main()
 {
     double** pptr;
-    double x0[2]={.0,.0};
-    //RungeKutta(f1,x0,8.0,2,500,pptr);
-    RungeKutta(f2,x0,8.0,1,20,pptr);
-    for(int i=0;i<21;++i){
+    double x0[3]={.0,1.0,1};
+    double x00[2]={0,.0};
+
+    RungeKutta(f1,x0,1.0,2,30,pptr);
+    for(int i=0;i<31;++i){
+
+        for(int k=0;k<3;++k){
+            std::cout<<pptr[i][k]<<"\t";
+        }
+        std::cout<<"\n";
+    }
+
+    for(int i=0;i<31;++i){
+        delete [] pptr[i];
+    }
+    delete [] pptr;
+
+    RungeKutta(f2,x00,5.0,1,30,pptr);
+    for(int i=0;i<31;++i){
 
         for(int k=0;k<2;++k){
             std::cout<<pptr[i][k]<<"\t";
@@ -24,9 +49,8 @@ int main()
         std::cout<<"\n";
     }
 
-    for(int i=0;i<21;++i){
+    for(int i=0;i<31;++i){
         delete [] pptr[i];
     }
-    delete [] pptr;
     return 0;
 }
