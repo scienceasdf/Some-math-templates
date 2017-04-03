93ac7ba9fcb1c0637e24ccb2b0fd1e2aaeedb266
scienceasdf <scienceasdf@126.com>
2017-04-03 20:13:03 +0800

    Update transformController.cpp

--- spacecraft/transformController.cpp
+++ spacecraft/transformController.cpp
@@ -13,7 +13,7 @@ TransformController::TransformController(QObject *parent)
         }
     }
     tensor(0,0)=80.0;
-    tensor(1,1)=12.0;
+    tensor(1,1)=10.0;
     tensor(2,2)=80.0;
 
     angularM[0]=.0;
@@ -29,10 +29,24 @@ TransformController::TransformController(QObject *parent)
 
     tensor=temp.transpose()*tensor*temp;
     omega=tensor.inverse()*angularM;
-    print(omega);
-    cosineMat=temp;
+    omega=temp*omega;
+    vec3 am=tensor*(temp.transpose()*omega);
+    print(am);
+    cosineMat=temp.transpose();
 
-    double T=omega.transpose()*(tensor*omega);
+    for(int i=0;i<3;++i){
+        for(int j=0;j<3;++j){
+            tensor(i,j)=.0;
+        }
+    }
+    tensor(0,0)=80.0;
+    tensor(1,1)=10.0;
+    tensor(2,2)=80.0;
+    am=cosineMat*tensor*cosineMat.transpose()*(cosineMat*omega);
+    print(am);
+
+    //double T=omega.transpose()*(tensor*omega);
+    double T=(cosineMat*omega).transpose()*(tensor*(cosineMat*omega));
     qDebug()<<T<<"\n";
 
 }
@@ -53,7 +67,7 @@ Qt3DCore::QTransform *TransformController::target() const
 void TransformController::setTime(float time)
 {
     if (!qFuzzyCompare(time, m_time)) {
-        dt=(time-m_time)/5000000.0;//1000.0;
+        dt=(time-m_time)/12000000.0;//1000.0;
         //dt=.01;
         qDebug()<<dt<<"\t";
         m_time=time;
@@ -70,7 +84,7 @@ float TransformController::time() const
 void TransformController::updateQuat()
 {
     //m_quat=QQuaternion::fromAxisAndAngle(1,2,3,m_time);
-    static mat33 tensor_;
+    /*static mat33 tensor_;
     static vec3 ome;
     static vec3 omega_;
     static mat33 A;
@@ -104,6 +118,30 @@ void TransformController::updateQuat()
     qDebug()<<theta<<"\t"<<T<<"\n";
 
     m_quat=fromMat33(cosineMat);
+    m_target->setRotation(m_quat);*/
+
+    //static mat33 A;
+    static double x=.0;
+    step(x,omega,cosineMat,dt);
+    m_quat=fromMat33(cosineMat.transpose());
     m_target->setRotation(m_quat);
+    qDebug()<<m_quat;
+
+
+    //print(omega);
+
+    //mat33 ts=
+    vec3 am=cosineMat*tensor*cosineMat.transpose()*(cosineMat*omega);
+    print(am);
+    double theta=angle(am,omega)*degPerRad;
+    qDebug()<<theta<<"deg\n";
+    //print(cosineMat);
+
+    //double T=omega.transpose()*(tensor*omega);
+    //double T=(cosineMat*omega).transpose()*(tensor*(cosineMat*omega));
+    //double T=omega.getX()*omega.getX()+omega.getZ()*omega.getZ();
+    //double theta=std::atan2(omega.getY(),pow(omega.getX()*omega.getX()+omega.getZ()*omega.getZ(),.5));
+    //double theta=std::atan2(v2.getY(),pow(v2.getX()*v2.getX()+v2.getZ()*v2.getZ(),.5));
+    //qDebug()<<x<<T<<"\n";
 }
 
