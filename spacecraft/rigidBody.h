e7b7184047ad3bbe716baa6affe82534a2f84641
scienceasdf <scienceasdf@126.com>
2017-04-05 16:18:12 +0800

    Update rigidBody.h

--- spacecraft/rigidBody.h
+++ spacecraft/rigidBody.h
@@ -1,15 +1,19 @@
 #ifndef RIGIDBODY_H
 #define RIGIDBODY_H
 
+#include<functional>
+
 #include"squaternion.h"
+
+
 
 class rigidBody
 {
 public:
     rigidBody();
-    rigidBody(double Ix, double Iy, double Iz, mat33& cosineMat, vec3& omega):
+    rigidBody(double Ix, double Iy, double Iz, mat33& cosineMat, vec3& omega,std::function<vec3(vec3&,double)> mome):
         m_Ix(Ix),m_Iy(Iy),m_Iz(Iz), m_inertia(mat33::fromDiag(Ix,Iy,Iz)),
-        m_cosMat(cosineMat), m_omega(omega),m_time(.0) {}
+        m_cosMat(cosineMat), m_omega(omega),m_time(.0), moment(mome) {}
 
     void do_step(double dt);    //calculate the state after time dt
 
@@ -18,6 +22,8 @@ public:
     vec3 getOmega();            //expressed in the inertial frame
     mat33 getInertiaTensor();   //expressed in the inertial frame
     mat33 getCosineMat();
+
+
 
 private:
     //double m_mass;
@@ -28,7 +34,7 @@ private:
     vec3 m_omega;       //expressed in the body frame
     //vec3 m_speed;
     double m_time;
-    vec3 moment(vec3& omega,double t); //suppose M=M(omega,t), expressed in the body frame
+    std::function<vec3(vec3&,double)> moment; //suppose M=M(omega,t), expressed in the body frame
     //vec3 force(parameters);
 
     void func(double t, vec3& omega, mat33& cosMat, vec3& resVec, mat33& resMat);
